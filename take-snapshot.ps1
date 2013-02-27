1f8f8d09dfb65c6189f112d59a4c1a07344a7a35
jorgecarpio <jcarpio9@yahoo.com>
2013-02-27 11:57:44 -0800

    Can specify folder name now

--- take-snapshot.ps1
+++ take-snapshot.ps1
@@ -1,12 +1,33 @@
-if (!$args)
-{Write-Host 'Specifiy hostnames after script. e.g. take-snapshot.ps1 host1 host2'
-break}
+  <#
+    .SYNOPSIS 
+      Takes a host or hosts, or optionally a VMware Folder name and snapshots them.
+    .EXAMPLE
+     .\take-snapshot.ps1 host1
+     Takes a snapshot of host1
+
+     .\take-snapshot.ps1 host1, host2
+     Takes a snapshot of host1 and host2
+
+     .\take-snapshot.ps1 -folderName Accounting
+     Takes snapshots of all the VM's in Accounting
+  #>
+
+Param(
+  [string[]] $hosts,
+  [string]$folderName
+  )
 
 Set-PowerCLIConfiguration -InvalidCertificateAction "Ignore" -DefaultVIServerMode single -Confirm:$false
 
 Connect-VIServer -Server server_name -Protocol https -User domain\username -Password password
 
 $name = get-date -format f
+if ($hosts) {
+  foreach ($host in $hosts)
+    {New-Snapshot -VM $host -Name $name -Quiesce:$false}
+}
 
-foreach ($i in $args)
-{New-Snapshot -VM $i -Name $name -Quiesce:$false}
+if ($foldername) {
+  foreach ($host in Get-Folder $foldername | Get-VM)
+    {New-Snapshot -VM $host -Name $name -Quiesce:$false}
+}
